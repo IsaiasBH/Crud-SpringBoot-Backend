337fcde125f6d07cb2c7db9a8a6353d32762df64
IsaiasBH <103340875+IsaiasBH@users.noreply.github.com>
2022-05-13 08:56:26 -0500

    Add files via upload

---
+++ crud/src/main/java/com/example/crud/PersonaService.java
@@ -0,0 +1,10 @@
+package com.example.crud;
+import java.util.List;
+
+public interface PersonaService {
+    List<Persona>listar();
+    Persona listarId(int id);
+    Persona add(Persona p);
+    Persona edit(Persona p);
+    Persona delete(int id);
+}
